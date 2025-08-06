d7288bb7c315c1d11c54c6f404cd4edbf37ffe53
yaniChan17 <lovelylibres5@gmail.com>
2025-08-06 15:31:48 +0800

    newLovely

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Lovely Jana Niña Libres BSIT3 blk 3</Text>
+      <StatusBar style="auto" />npm
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
