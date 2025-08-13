d916b1552c5c8853fa2684f916ccb4e82238dd42
yaniChan17 <lovelylibres5@gmail.com>
2025-08-13 17:04:13 +0800

    Color Changer App

---
+++ ColorChangerApp.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+
+export default function ColorChangerApp(){
+    const [bgColor, setBgColor] = useState('white');
+
+    return(
+        <View style = {[styles.container, {backgroundColor:bgColor}]}>
+             <Button title='White'onPress={() => setBgColor('white')}/>
+               <Button title='Light Blue'onPress={() => setBgColor('lightblue')}/>
+               <Button title='Light Green'onPress={() => setBgColor('lightgreen')}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
